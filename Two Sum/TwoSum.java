102cafc29162f8a16390e2584f50bb5642f29f24
mohanraj.c@zohocorp.com <mohanraj.c@zohocorp.com>
2021-06-22 14:49:29 +0530

    soltuon updated

--- Two Sum/TwoSum.java
+++ Two Sum/TwoSum.java
@@ -13,16 +13,15 @@ class Solution {
 }
 
 // O(n)
-public int[] twoSum(int[] numbers, int target) {
-  int[] result = new int[2];
-  Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-  for (int i = 0; i < numbers.length; i++) {
-      if (map.containsKey(target - numbers[i])) {
-          result[1] = i;
-          result[0] = map.get(target - numbers[i]);
-          return result;
+class Solution {
+  public int[] twoSum(int[] numArray, int target) {
+      Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+      for( int i=0; i<numArray.length ; i++) {
+          if(map.containsKey(target - numArray[i])) {
+              return new int[] {map.get(target - numArray[i]), i};
+          }
+          map.put(numArray[i], i);
       }
-      map.put(numbers[i], i);
+      return new int[] {0,0};
   }
-  return result;
 }
