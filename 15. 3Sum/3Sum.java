b678d5ac55b7de93be13b9fe41502cb0b80bbdf3
mohanraj.c@zohocorp.com <mohanraj.c@zohocorp.com>
2021-07-23 12:17:59 +0530

    opitmised Solutionn #15

--- 15. 3Sum/3Sum.java
+++ 15. 3Sum/3Sum.java
@@ -19,3 +19,34 @@ class Solution {
       return res;
   }
 }
+// O(N*2) faster than 98% optimised
+class Solution {
+  public List<List<Integer>> threeSum(int[] nums) {
+      Arrays.sort(nums);
+      int n = nums.length;
+      List<List<Integer>> res = new LinkedList<>();
+      for(int i=0; i<n-2; i++){
+          if(i == 0 || (i>0 && nums[i] != nums[i-1])) {
+              if(nums[i] > 0)
+                  break;
+              int lo = i+1, hi = n-1, sum = 0 - nums[i];
+              while(lo < hi) {
+                  if(nums[lo] + nums[hi] == sum) {
+                      res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                      while(lo < hi && nums[lo] == nums[lo+1])
+                          lo++;
+                      while(lo < hi && nums[hi] == nums[hi-1])
+                          hi--;
+                      lo++;
+                      hi--;
+                  }
+                  else if(nums[lo] + nums[hi] < sum)
+                      lo++;
+                  else
+                      hi--;
+              }
+          }
+      }
+      return res;
+  }
+}
