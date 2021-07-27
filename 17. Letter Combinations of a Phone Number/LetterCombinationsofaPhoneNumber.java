9175bef698fa05206e12718b2c3980166ecbce0b
mohanraj.c@zohocorp.com <mohanraj.c@zohocorp.com>
2021-07-27 12:39:09 +0530

    Optimized using BFS algotithm

--- 17. Letter Combinations of a Phone Number/LetterCombinationsofaPhoneNumber.java
+++ 17. Letter Combinations of a Phone Number/LetterCombinationsofaPhoneNumber.java
@@ -32,3 +32,22 @@ class Solution {
       return res;
   }
 }
+
+// Optimized using BFS algotithm in single queue
+class Solution {
+  public List<String> letterCombinations(String digits) {
+      LinkedList<String> res = new LinkedList<String>();
+      if(digits.isEmpty()) return res;
+      String[] mapping = new String[]{"0","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+      res.add("");
+      for(int i=0; i<digits.length(); i++) {
+          int x = Character.getNumericValue(digits.charAt(i));
+          while(res.peek().length() == i) {
+              String t = res.remove();
+              for(char c: mapping[x].toCharArray())
+                  res.add(t+c);
+          }
+      }
+      return res;
+  }
+}
