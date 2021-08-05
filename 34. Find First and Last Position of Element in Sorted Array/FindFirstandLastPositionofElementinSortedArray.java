5a17f905a1956b50fbe55ccf88d74cdaf035ba33
mohanraj.c@zohocorp.com <mohanraj.c@zohocorp.com>
2021-08-05 16:02:31 +0530

    #34 solution updated

--- 34. Find First and Last Position of Element in Sorted Array/FindFirstandLastPositionofElementinSortedArray.java
+++ 34. Find First and Last Position of Element in Sorted Array/FindFirstandLastPositionofElementinSortedArray.java
@@ -23,31 +23,30 @@ class Solution {
 }
 
 //Solution with annotation
-public class Solution {
-	public int[] searchRange(int[] A, int target) {
-		int start = Solution.firstGreaterEqual(A, target);
-		if (start == A.length || A[start] != target) {
-			return new int[]{-1, -1};
-		}
-		return new int[]{start, Solution.firstGreaterEqual(A, target + 1) - 1};
-	}
-
+class Solution {
 	//find the first number that is greater than or equal to target.
 	//could return A.length if target is greater than A[A.length-1].
-	//actually this is the same as lower_bound in C++ STL.
-	private static int firstGreaterEqual(int[] A, int target) {
-		int low = 0, high = A.length;
-		while (low < high) {
-			int mid = low + ((high - low) >> 1);
+	int findBound(int[] nums, int target) {
+			int left = 0, right = nums.length;
+			while(left < right) {
+					int mid = left + (right - left) / 2;
 			//low <= mid < high
-			if (A[mid] < target) {
-				low = mid + 1;
-			} else {
-				//should not be mid-1 when A[mid]==target.
-				//could be mid even if A[mid]>target because mid<high.
-				high = mid;
+					if(nums[mid] < target) {
+							left = mid + 1;
+					} else {
+			//should not be mid-1 when A[mid]==target.
+			//could be mid even if A[mid]>target because mid<high.
+							right = mid;
+					}
 			}
-		}
-		return low;
+			return left;
 	}
-}
+	
+	public int[] searchRange(int[] nums, int target) {
+			int start = findBound(nums, target);
+			if(start == nums.length || nums[start] != target) {
+					return new int[]{-1, -1};
+			}
+			return new int[]{start, findBound(nums, target + 1) - 1};
+	}
+	}
