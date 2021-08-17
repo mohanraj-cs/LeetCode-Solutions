99c49a1d2ea3c434b9dc4f2da3c9325716138594
mohanraj.c@zohocorp.com <mohanraj.c@zohocorp.com>
2021-08-17 17:31:18 +0530

    #55 O(n)

---
+++ 55. Jump Game/JumpGame.java
@@ -0,0 +1,19 @@
+// O(n) Greedy Algo
+public boolean canJump(int[] nums) {
+    int n = nums.length, maxReachable = 0, i;
+    for(i = 0; i < n && i <= maxReachable; i++) 
+        maxReachable = Math.max(i + nums[i], maxReachable);
+    return i == n;
+}
+
+
+// Optimzed with pre condition
+public boolean canJump(int[] nums) {
+  int n = nums.length, maxReachable = 0;
+  for(int i = 0; i < n && i <= maxReachable; i++) {
+      maxReachable = Math.max(i + nums[i], maxReachable);
+      if(maxReachable >= n-1)
+          return true;
+  }
+  return false;
+}
