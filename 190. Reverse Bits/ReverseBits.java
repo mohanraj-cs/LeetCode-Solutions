8a9e1f1f6e1e12976980ca3e44a5d47608a449f5
Mohan <mohanraj.c@zohocorp.com>
2021-09-17 19:53:44 +0530

    190

---
+++ 190. Reverse Bits/ReverseBits.java
@@ -0,0 +1,15 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        if(n == 0)
+            return 0;
+        int res = 0;
+        for(int i = 0; i < 32; i++) {
+            res = res << 1;
+            if((n & 1) == 1)
+                res = res + 1;
+            n = n >> 1;
+        }
+        return res;
+    }
+}
