50763c4f0e65d22905e6e059fdbd4ee6ee2143cc
Mohan Raj <mohanraj746007@gmail.com>
2021-09-23 19:49:14 +0530

    20

--- 20. Valid Parentheses/ValidParentheses.java
+++ 20. Valid Parentheses/ValidParentheses.java
@@ -1,18 +1,15 @@
 //14 line || java || 1 ms || 99% faster || Optimized solution
-
-class Solution {
-	public boolean isValid(String s) {
-			Stack<Character> stack = new Stack<Character>();
-			for(Character c : s.toCharArray()) {
-					if(c == '(')
-							stack.push(')');
-					else if(c == '{')
-							stack.push('}');
-					else if(c == '[')
-							stack.push(']');
-					else if(stack.isEmpty() || stack.pop() != c)
-							return false;
-			}
-			return stack.isEmpty();
+public boolean isValid(String s) {
+	Stack < Character > stack = new Stack < Character > ();
+	for (Character c: s.toCharArray()) {
+		if (c == '(')
+			stack.push(')');
+		else if (c == '{')
+			stack.push('}');
+		else if (c == '[')
+			stack.push(']');
+		else if (stack.isEmpty() || stack.pop() != c)
+			return false;
 	}
+	return stack.isEmpty();
 }
