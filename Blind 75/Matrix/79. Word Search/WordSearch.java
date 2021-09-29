352ab04e8ce040478a77f7895e7e8dbe54b33123
Mohan Raj <mohanraj746007@gmail.com>
2021-09-29 19:56:27 +0530

    75

---
+++ Blind 75/Matrix/79. Word Search/WordSearch.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++)
+            for (int j = 0; j < board[0].length; j++) {
+                if (dfs(board, i, j, word, 0))
+                    return true;
+            }
+        return false;
+    }
+    
+    private boolean dfs(char[][] board, int i, int j, String word, int ind) {
+        if (ind == word.length()) 
+            return true;
+        if (i > board.length - 1 || i < 0 || j < 0 || j > board[0].length - 1 || board[i][j] != word.charAt(ind))
+            return false;
+        board[i][j] = '*'; // use '*' to represent this cell is visited
+        boolean result = dfs(board, i - 1, j, word, ind + 1) ||
+            dfs(board, i, j - 1, word, ind + 1) ||
+            dfs(board, i, j + 1, word, ind + 1) ||
+            dfs(board, i + 1, j, word, ind + 1);
+        board[i][j] = word.charAt(ind);
+        return result;
+    }
+}
