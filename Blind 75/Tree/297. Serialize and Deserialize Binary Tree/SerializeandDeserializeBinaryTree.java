3bb15bb9ddd68579a7f399c59ef5eb137e4c94a6
Mohan Raj <mohanraj746007@gmail.com>
2021-10-12 14:15:37 +0530

    297

---
+++ Blind 75/Tree/297. Serialize and Deserialize Binary Tree/SerializeandDeserializeBinaryTree.java
@@ -0,0 +1,33 @@
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        return serial(new StringBuilder(), root).toString();
+    }
+
+    // Generate preorder string
+    private StringBuilder serial(StringBuilder str, TreeNode root) {
+        if (root == null)
+            return str.append("#");
+        str.append(root.val).append(",");
+        serial(str, root.left).append(",");
+        serial(str, root.right);
+        return str;
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        return deserial(new LinkedList < > (Arrays.asList(data.split(","))));
+    }
+
+    // Use queue to simplify position move
+    private TreeNode deserial(Queue < String > q) {
+        String val = q.poll();
+        if ("#".equals(val))
+            return null;
+        TreeNode root = new TreeNode(Integer.valueOf(val));
+        root.left = deserial(q);
+        root.right = deserial(q);
+        return root;
+    }
+}
