67c94384d7083da6d91a9424910f533925fd0bcb
Mohan Raj <mohanraj746007@gmail.com>
2021-10-24 17:19:03 +0530

    647

--- Blind 75/String/647. Palindromic Substrings/Palindromic Substrings.java
+++ Blind 75/String/647. Palindromic Substrings/Palindromic Substrings.java
@@ -1,19 +1,17 @@
-class Solution {
-    public int countSubstrings(String s) {
-        int res = 0;
-        for (int i = 0; i < s.length(); i++) {
-            res += extractPalindrome(s, i, i); //odd length
-            res += extractPalindrome(s, i, i + 1); //even length
-        }
-        return res;
+public int countSubstrings(String s) {
+    int res = 0;
+    for (int i = 0; i < s.length(); i++) {
+        res += countPalindromes(s, i, i); //odd length
+        res += countPalindromes(s, i, i + 1); //even length
     }
-    public int extractPalindrome(String s, int left, int right) {
-        int count = 0;
-        while (left >= 0 && right < s.length() && (s.charAt(left) == s.charAt(right))) {
-            left--;
-            right++;
-            count++;
-        }
-        return count;
+    return res;
+}
+public int countPalindromes(String s, int l, int r) {
+    int count = 0;
+    while (l >= 0 && r < s.length() && (s.charAt(l) == s.charAt(r))) {
+        l--;
+        r++;
+        count++;
     }
+    return count;
 }
