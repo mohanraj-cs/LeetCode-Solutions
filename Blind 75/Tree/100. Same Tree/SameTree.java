1f59381fe9decea6b6f0afca9bc19bde8157b5b9
Mohan Raj <mohanraj746007@gmail.com>
2022-02-19 15:30:44 +0530

    new slution

--- Blind 75/Tree/100. Same Tree/SameTree.java
+++ Blind 75/Tree/100. Same Tree/SameTree.java
@@ -1,8 +1,21 @@
+//Optimized trick
+// from prev solution, the first 2 lines can be combined into 1 line:
+// if (p == null || q == null) return (p == q);
+public boolean isSameTree(TreeNode p, TreeNode q) {
+	if (p == null || q == null)
+		return (p == q);
+	if(p.val == q.val)
+		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+	return false;
+}
+
+
+
 //Recusursion
 public boolean isSameTree(TreeNode p, TreeNode q) {
-	if(p == null && q == null) 
+	if(p == null && q == null)
 		return true;
-	if(p == null || q == null) 
+	if(p == null || q == null)
 		return false;
 	if(p.val == q.val)
 		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
@@ -11,22 +24,22 @@ public boolean isSameTree(TreeNode p, TreeNode q) {
 
 //Commented Version
 public boolean isSameTree(TreeNode p, TreeNode q) {
-    
+
     // Equal nullity denotes that this branch is the same (local equality)
     // This is a base case, but also handles being given two empty trees
-    if (p == null && q == null) 
+    if (p == null && q == null)
 		return true;
-    
+
     // Unequal nullity denotes that the trees aren't the same
     // Note that we've already ruled out equal nullity above
-    else if (p == null || q == null) 
+    else if (p == null || q == null)
 		return false;
-        
+
     // Both nodes have values; descend iff those values are equal
     // "&&" here allows for any difference to overrule a local equality
-    else if (p.val == q.val) 
+    else if (p.val == q.val)
 		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-    
+
     // If we're here, both nodes have values, and they're unequal, so the trees aren't the same
     return false;
 }
@@ -34,4 +47,3 @@ public boolean isSameTree(TreeNode p, TreeNode q) {
 //Improve replace first two check
 	// if (p == NULL || q == NULL)
 	// 	return (p == q);
-
