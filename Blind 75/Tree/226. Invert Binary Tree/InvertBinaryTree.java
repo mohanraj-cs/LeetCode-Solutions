956a9e5aac3ea342548b8e6ae1ece3d1bb23a8fb
Mohan Raj <mohanraj746007@gmail.com>
2022-02-21 13:24:06 +0530

    new solution

--- Blind 75/Tree/226. Invert Binary Tree/InvertBinaryTree.java
+++ Blind 75/Tree/226. Invert Binary Tree/InvertBinaryTree.java
@@ -11,8 +11,10 @@ public TreeNode invertTree(TreeNode root) {
 }
 
 
-// The above solution is correct, but it is also bound to the application stack, which means that it's no so much scalable 
+// The above solution is correct, but it is also bound to the application stack, which means that it's no so much scalable
 // - (you can find the problem size that will overflow the stack and crash your application), so more robust solution would be to use stack data structure.
+// LIFO
+// https://stackoverflow.com/questions/12524826/why-should-i-use-deque-over-stack
 public TreeNode invertTree(TreeNode root) {
   if (root == null) {
     return null;
@@ -23,9 +25,9 @@ public TreeNode invertTree(TreeNode root) {
 
   while (!stack.isEmpty()) {
     TreeNode node = stack.pop();
-    TreeNode left = node.left;
+    TreeNode temp = node.left;
     node.left = node.right;
-    node.right = left;
+    node.right = temp;
 
     if (node.left != null) {
       stack.push(node.left);
@@ -35,4 +37,31 @@ public TreeNode invertTree(TreeNode root) {
     }
   }
   return root;
+}
+
+// Finally we can easly convert the above solution to BFS - or so called level order traversal.
+// FIFO
+public TreeNode invertTree(TreeNode root) {
+
+    if (root == null) {
+        return null;
+    }
+
+    final Queue < TreeNode > queue = new LinkedList < > ();
+    queue.offer(root);
+
+    while (!queue.isEmpty()) {
+        final TreeNode node = queue.poll();
+        final TreeNode temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+
+        if (node.left != null) {
+            queue.offer(node.left);
+        }
+        if (node.right != null) {
+            queue.offer(node.right);
+        }
+    }
+    return root;
 }
