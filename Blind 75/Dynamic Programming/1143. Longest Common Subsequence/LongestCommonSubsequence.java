8e9b741790b7b96a15d1331d9b3253053c3d241c
Mohan Raj <mohanraj746007@gmail.com>
2022-03-13 17:30:26 +0530

    dp

--- Blind 75/Dynamic Programming/1143. Longest Common Subsequence/LongestCommonSubsequence.java
+++ Blind 75/Dynamic Programming/1143. Longest Common Subsequence/LongestCommonSubsequence.java
@@ -23,3 +23,23 @@ public int longestCommonSubsequence(String s1, String s2) {
 // Space Optimization
 
 // Obviously, the code in method 1 only needs information of previous row to update current row. So we just use a two-row 2D array to save and update the matching results for chars in s1 and s2.
+
+
+// Brute Force
+// TLE - not feassible solution
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        return longestCommonSubsequence(text1, text2, 0, 0);
+    }
+
+    private int longestCommonSubsequence(String text1, String text2, int i, int j) {
+        if (i == text1.length() || j == text2.length())
+            return 0;
+        if (text1.charAt(i) == text2.charAt(j))
+            return 1 + longestCommonSubsequence(text1, text2, i + 1, j + 1);
+        else
+            return Math.max(
+                longestCommonSubsequence(text1, text2, i + 1, j),
+                longestCommonSubsequence(text1, text2, i, j + 1)
+            );
+    }
