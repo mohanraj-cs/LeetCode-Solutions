6ae67dc7ac76478e4c0cb8eac147ce620b70fa53
Mohan Raj <mohanraj746007@gmail.com>
2022-03-15 14:08:14 +0530

    optimised solution

--- Blind 75/Dynamic Programming/139. Word Break/WordBreak.java
+++ Blind 75/Dynamic Programming/139. Word Break/WordBreak.java
@@ -1,16 +1,119 @@
 //DP | O(n^2)
-public boolean wordBreak(String s, List<String> wordDict) {
+// leetcode
+// -
+// leetcode
+//  -
+// --
+// leetcode
+//   -
+//  --
+// ---
+// leetcode
+//    -
+//   --
+//  ---
+// ----     !!! leet
+// leetcode
+//     -
+//    --
+//   ---
+//  ----
+// -----
+// leetcode
+//      -
+//     --
+//    ---
+//   ----
+//  -----
+// ------
+// leetcode
+//       -
+//      --
+//     ---
+//    ----
+//   -----
+//  ------
+// -------
+// leetcode
+//        -
+//       --
+//      ---
+//     ----  !!! code
+// All the lines below won't run b/c of break;
+// Index 4 is a valid start index, and index 4 - index 7 make up the word `code`
+// We can break once we've found any connecting word because
+// all we're trying to do is find SOME valid way to connect to s[i].
+// Once we've found SOME word that does that, there's no need to continue checking.
+//    -----
+//   ------
+//  -------
+// --------
+// just want to add some comments for the time complexity:
+// First DP: [length of s][size of dict][avg length of words in dict]
+// Second DP: [length of s]^3
+//
+// BTW, for this kind of problem, which time complexity is [length of s][size of dict][avg length of words in dict]. We can usually remove [size of dict] by using Tire, remove [avg length of words in dict] by KMP, and what's more, remove both [size of dict] and [avg length of words in dict] by AC-automata. And of course these are all doable for this problem.
+// This is just a insight for people who want to think deeper about this problem, hope it can help you :)
+// Average O(n^2)
+public boolean wordBreak(String s, List < String > wordDict) {
     boolean[] dp = new boolean[s.length() + 1];
-	Set<String> set = new HashSet<>(wordDict);
+    Set < String > set = new HashSet < > (wordDict);
 
     dp[0] = true;
     for (int i = 1; i <= s.length(); i++) {
         for (int j = i - 1; j >= 0; j--) {
-			if(dp[j] && set.contains(s.substring(j, i))){
-				dp[i] = true;
-				break;
-			}
+            if (dp[j] && set.contains(s.substring(j, i))) {
+                dp[i] = true;
+                break;
+            }
         }
     }
     return dp[s.length()];
+}
+
+// Recoursion with mem - O(n^3)
+class Solution {
+    public boolean wordBreak(String s, List < String > wordDict) {
+        Boolean[] mem = new Boolean[s.length()];
+        Set < String > set = new HashSet < > (wordDict);
+        return wordBreak(s, 0, set, mem);
+    }
+
+    private boolean wordBreak(String s, int p, Set < String > set, Boolean[] mem) {
+        int n = s.length();
+        if (p == n) {
+            return true;
+        }
+        if (mem[p] != null) {
+            return mem[p];
+        }
+        for (int i = p + 1; i <= n; i++) {
+            if (set.contains(s.substring(p, i)) && wordBreak(i, s, set, mem)) {
+                return mem[p] = true;
+            }
+        }
+        return mem[p] = false;
+    }
+}
+
+// Recursion - TLE - not feassible
+class Solution {
+    public boolean wordBreak(String s, List < String > wordDict) {
+        // put all words into a hashset
+        Set < String > set = new HashSet < > (wordDict);
+        return wb(s, set);
+    }
+
+    private boolean wb(String s, Set < String > set) {
+        int len = s.length();
+        if (len == 0) {
+            return true;
+        }
+        for (int i = 1; i <= len; ++i) {
+            if (set.contains(s.substring(0, i)) && wb(s.substring(i), set)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- Blind 75/Dynamic Programming/377. Combination Sum IV/CombinationSumIV.java
+++ Blind 75/Dynamic Programming/377. Combination Sum IV/CombinationSumIV.java
@@ -1,38 +1,88 @@
-// bottom-up approach || DP
-
+// bottom-up approach - DP
+// https://www.youtube.com/watch?v=PomVtsn7ZoM
 public int combinationSum4(int[] nums, int target) {
-    int[] comb = new int[target + 1];
-    comb[0] = 1;
-    for (int i = 1; i < comb.length; i++) {
+    int[] dp = new int[target + 1];
+    dp[0] = 1;
+    for (int i = 1; i <= target; i++) {
         for (int j = 0; j < nums.length; j++) {
             if (i - nums[j] >= 0) {
-                comb[i] += comb[i - nums[j]];
+                dp[i] += dp[i - nums[j]];
             }
         }
     }
-    return comb[target];
+    return dp[target];
 }
 
-// Top Down approach 
-private int[] dp;
+// order-1:
+//
+// for each sum in dp[]
+//     for each num in nums[]
+//         if (sum >= num)
+//             dp[sum] += dp[sum-num];
+// order-2:
+//
+// for each num in nums[]
+//     for each sum in dp[]  >= num
+//         dp[sum] += dp[sum-num];
+// order-1 is used to calculate the number of combinations considering different sequences
+// order-2 is used to calculate the number of combinations NOT considering different sequences
+// Give an example nums[] = {1, 2, 3}, target = 4
+// order-1 considers the number of combinations starting from 1, 2, and 3, respectively, so all sequences are considered as the graph below.
+//
+// 1 --> 1 --> 1 --> 1 --> (0)
+// 1 --> 1 --> 2 --> (0)
+// 1 --> 2 --> 1 --> (0)
+// 1 --> 3 --> (0)
+//
+// 2 --> 1 --> 1 --> (0)
+// 2 --> 2 --> (0)
+//
+// 3 --> 1 --> (0)
+//
+// order-2 considers the number of combinations starting from 0 (i.e., not picking anyone), and the index of the num picked next must be >= the index of previous picked num, so different sequences are not considered, as the graph below.
+//
+// (0) --> 1 --> 1 --> 1 --> 1
+// (0) --> 1 --> 1 --> 2
+// (0) --> 1 --> 3
+// (0) --> 2 --> 2
 
+// ------------------------------
+// Top Down approach - recursion with mem cache
+class Solution {
+    private int[] dp;
+
+    public int combinationSum4(int[] nums, int target) {
+        dp = new int[target + 1];
+        Arrays.fill(dp, -1);
+        dp[0] = 1;
+        return helper(nums, target);
+    }
+
+    private int helper(int[] nums, int target) {
+        if (dp[target] != -1) {
+            return dp[target];
+        }
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (target >= nums[i]) {
+                res += helper(nums, target - nums[i]);
+            }
+        }
+        dp[target] = res;
+        return res;
+    }
+}
+
+// Recursion
 public int combinationSum4(int[] nums, int target) {
-    dp = new int[target + 1];
-    Arrays.fill(dp, -1);
-    dp[0] = 1;
-    return helper(nums, target);
-}
-
-private int helper(int[] nums, int target) {
-    if (dp[target] != -1) {
-        return dp[target];
+    if (target == 0) {
+        return 1;
     }
     int res = 0;
     for (int i = 0; i < nums.length; i++) {
         if (target >= nums[i]) {
-            res += helper(nums, target - nums[i]);
+            res += combinationSum4(nums, target - nums[i]);
         }
     }
-    dp[target] = res;
     return res;
 }
