b6f2434f1cd5193f77c01d4edb9e71f72394a019
Mohan Raj <mohanraj746007@gmail.com>
2022-03-20 15:55:26 +0530

    solutio  update

--- Blind 75/Dynamic Programming/91. Decode Ways/DecodeWays.java
+++ Blind 75/Dynamic Programming/91. Decode Ways/DecodeWays.java
@@ -1,41 +1,46 @@
-// 1- Recursion O(2^n). A char may be decoded alone or by pairing with the next char.
+// Constant Space
 public int numDecodings(String s) {
-    return s.length() == 0 ? 0 : numDecodings(0, s);
-}
-private int numDecodings(int p, String s) {
-    int n = s.length();
-    if (p == n)
-        return 1;
-    if (s.charAt(p) == '0')
+    if (s == null || s.length() == 0) {
         return 0;
-    int res = numDecodings(p + 1, s);
-    if (p < n - 1 && (s.charAt(p) == '1' || (s.charAt(p) == '2' && s.charAt(p + 1) < '7')))
-        res += numDecodings(p + 2, s);
-    return res;
+    }
+    int prev1 = 1, prev2 = 0, n = s.length();
+    for (int i = n - 1; i >= 0; i--) {
+        int dp = s.charAt(i) == '0' ? 0 : prev1;
+        if (i < n - 1 && (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i + 1) < '7')))
+            dp += prev2;
+        prev2 = prev1;
+        prev1 = dp;
+    }
+    return prev1;
 }
 
-// 2- Memoization O(n)
+// Top Down - DP
 public int numDecodings(String s) {
-    int n = s.length();
-    Integer[] mem = new Integer[n];
-    return s.length() == 0 ? 0 : numDecodings(0, s, mem);
-}
-private int numDecodings(int p, String s, Integer[] mem) {
-    int n = s.length();
-    if (p == n)
-        return 1;
-    if (s.charAt(p) == '0')
+    if (s == null || s.length() == 0) {
         return 0;
-    if (mem[p] != null)
-        return mem[p];
-    int res = numDecodings(p + 1, s, mem);
-    if (p < n - 1 && (s.charAt(p) == '1' || (s.charAt(p) == '2' && s.charAt(p + 1) < '7')))
-        res += numDecodings(p + 2, s, mem);
-    return mem[p] = res;
+    }
+    int n = s.length();
+    int[] dp = new int[n + 1];
+    dp[0] = 1;
+    dp[1] = s.charAt(0) != '0' ? 1 : 0;
+    for (int i = 2; i <= n; i++) {
+        int first = Integer.valueOf(s.substring(i - 1, i));
+        int second = Integer.valueOf(s.substring(i - 2, i));
+        if (first >= 1) {
+            dp[i] += dp[i - 1];
+        }
+        if (second >= 10 && second <= 26) {
+            dp[i] += dp[i - 2];
+        }
+    }
+    return dp[n];
 }
 
-// 3 - DP 
+// Bottom Up - DP
 public int numDecodings(String s) {
+    if (s == null || s.length() == 0) {
+        return 0;
+    }
     int n = s.length();
     int[] dp = new int[n + 1];
     dp[n] = 1;
@@ -49,15 +54,47 @@ public int numDecodings(String s) {
     return dp[0];
 }
 
-// 4 - Constant Space
-public int numDecodings(String s) {
-    int dp1 = 1, dp2 = 0, n = s.length();
-    for (int i = n - 1; i >= 0; i--) {
-        int dp = s.charAt(i) == '0' ? 0 : dp1;
-        if (i < n - 1 && (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i + 1) < '7')))
-            dp += dp2;
-        dp2 = dp1;
-        dp1 = dp;
+// Memoization O(n)
+class Solution {
+    private Integer[] mem;
+
+    public int numDecodings(String s) {
+        if (s.length() == 0)
+            return 0;
+        mem = new Integer[s.length()];
+        return numDecodings(s, 0);
     }
-    return dp1;
+
+    private int numDecodings(String s, int p) {
+        if (p == s.length())
+            return 1;
+        if (s.charAt(p) == '0')
+            return 0;
+        if (mem[p] != null)
+            return mem[p];
+        int res = numDecodings(s, p + 1);
+        if (p < s.length() - 1 && (s.charAt(p) == '1' || (s.charAt(p) == '2' && s.charAt(p + 1) < '7')))
+            res += numDecodings(s, p + 2);
+        return mem[p] = res;
+    }
+}
+
+// Recursion O(2^n). A char may be decoded alone or by pairing with the next char.
+class Solution {
+    public int numDecodings(String s) {
+        if (s.length() == 0)
+            return 0;
+        return numDecodings(s, 0);
+    }
+
+    private int numDecodings(String s, int p) {
+        if (p == s.length())
+            return 1;
+        if (s.charAt(p) == '0')
+            return 0;
+        int res = numDecodings(s, p + 1);
+        if (p < s.length() - 1 && (s.charAt(p) == '1' || (s.charAt(p) == '2' && s.charAt(p + 1) < '7')))
+            res += numDecodings(s, p + 2);
+        return res;
+    }
 }
