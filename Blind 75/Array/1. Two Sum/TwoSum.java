d1340c904f8f8f3f61a0921a154f29f57cb3fb56
Mohan Raj <mohanraj746007@gmail.com>
2022-04-05 12:27:41 +0530

    comit solut

--- Blind 75/Array/1. Two Sum/TwoSum.java
+++ Blind 75/Array/1. Two Sum/TwoSum.java
@@ -1,21 +1,21 @@
+// O(n)
+public int[] twoSum(int[] nums, int target) {
+    Map < Integer, Integer > map = new HashMap < Integer, Integer > ();
+    for (int i = 0; i < nums.length; i++) {
+        if (map.containsKey(nums[i])) {
+            return new int[] { map.get(nums[i]), i };
+        }
+        map.put(target - nums[i], i);
+    }
+    return new int[] { 0, 0 };
+}
+
 //Brute force approach O(n*2)
 public int[] twoSum(int[] numbers, int target) {
     for (int i = 0; i < numbers.length - 1; i++)
         for (int j = i + 1; j < numbers.length; j++)
-            if (numbers[i] + numbers[j] == target)
-                return new int[] {i, j };
-    return new int[] {0, 0 };
-}
-
-// O(n)
-public int[] twoSum(int[] numArray, int target) {
-    Map < Integer, Integer > map = new HashMap < Integer, Integer > ();
-    for (int i = 0; i < numArray.length; i++) {
-        if (map.containsKey(target - numArray[i])) {
-            return new int[] {map.get(target - numArray[i]), i };
-            
-        }
-        map.put(numArray[i], i);
-    }
-    return new int[] { 0, 0 };
+            if (numbers[i] + numbers[j] == target){
+                return new int[] {i, j};
+            }
+    return new int[] {0, 0};
 }
