44366a4effab1105a09944123eeaaba11533b862
Mohan Raj <mohanraj746007@gmail.com>
2022-04-07 20:07:23 +0530

    solution

--- Blind 75/Array/53. Maximum Subarray/MaximumSubarray.java
+++ Blind 75/Array/53. Maximum Subarray/MaximumSubarray.java
@@ -1,10 +1,10 @@
 class Solution {
-  public int maxSubArray(int[] nums) {
-     int maxSoFar=nums[0], maxEndingHere=nums[0];
-     for (int i = 1; i < nums.length; i++) {
-         maxEndingHere= Math.max(maxEndingHere + nums[i], nums[i]);
-         maxSoFar=Math.max(maxSoFar, maxEndingHere);	
-     }
-     return maxSoFar;
-  }
+    public int maxSubArray(int[] nums) {
+        int maxCurr = nums[0], maxSoFar = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            maxCurr = Math.max(nums[i], nums[i] + maxCurr);
+            maxSoFar = Math.max(maxCurr, maxSoFar);
+        }
+        return maxSoFar;
+    }
 }
