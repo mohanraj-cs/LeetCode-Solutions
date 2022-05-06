1b68c199c7f3a78bf1204e7a6b2033832cf046ab
Mohan Raj <mohanraj746007@gmail.com>
2022-05-06 21:42:17 +0530

    solution

--- Blind 75/Linked List/143. Reorder List/ReorderList.java
+++ Blind 75/Linked List/143. Reorder List/ReorderList.java
@@ -1,3 +1,52 @@
+//Method 1
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null)
+            return;
+        // Find the middle node
+        // step 1. cut the list to two halves
+        // Go fast one step ahead, so gap will be sufficient for odd size
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // step 2. reverse the 2nd half
+        ListNode head2 = reverse(slow.next);
+        slow.next = null;
+
+        // step 3. merge the two halves
+        merge(head, head2);
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode preHead = null;
+        ListNode handler = head;
+        while (handler != null) {
+            ListNode recordNext = handler.next; //save next
+            handler.next = preHead; // reverse
+            preHead = handler; // advance prev
+            handler = recordNext; // advance currHead
+        }
+        // Now head will be null at the End of node
+        return preHead; // new Head
+    }
+
+    private void merge(ListNode head1, ListNode head2) {
+        while (head1 != null && head2 != null) {
+            ListNode tmp1 = head1.next;
+            ListNode tmp2 = head2.next;
+            head2.next = head1.next;
+            head1.next = head2;
+            head1 = tmp1;
+            head2 = tmp2;
+        }
+    }
+}
+
+
+//Method 2
 class Solution {
     public void reorderList(ListNode head) {
         if (head == null || head.next == null)
